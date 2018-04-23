99ddce65eede33dabdf5f676422c5ed33ff5d8db
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Drawing
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+	Protected Sub ASPxColorEdit1_ValueChanged(ByVal sender As Object, ByVal e As EventArgs)
+		ASPxHtmlEditor1.Styles.DesignViewArea.BackColor = ASPxColorEdit1.Color
+		ASPxHtmlEditor1.Styles.PreviewArea.BackColor = ASPxHtmlEditor1.Styles.DesignViewArea.BackColor
+	End Sub
+	Protected Sub ASPxButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+		preview.Visible = True
+		preview.Attributes("src") = "GenerateDocument.aspx"
+		Session("color") = ColorTranslator.ToHtml(ASPxHtmlEditor1.Styles.DesignViewArea.BackColor)
+		Session("html") = ASPxHtmlEditor1.Html
+	End Sub
+End Class
---
+++ VB/WebSite/GenerateDocument.aspx.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class GenerateDocument
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		If Session("color") IsNot Nothing Then
+			body1.Style.Add(HtmlTextWriterStyle.BackgroundColor, Session("color").ToString())
+			body1.InnerHtml = Session("html").ToString()
+		End If
+	End Sub
+End Class
